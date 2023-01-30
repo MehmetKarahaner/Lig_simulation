973e78181586db93ab6b9f209d041fb7cb0334cf
MehmetKarahaner <mehmetkarahaner@hotmail.com>
2023-01-30 11:47:19 +0300

    Degisiklik yapıldı

--- src/Oyuncular.java
+++ src/Oyuncular.java
@@ -8,6 +8,7 @@ public class Oyuncular {
         this.savunmaGucu = savunmaGucu;
     }
     public void oyuncuBilgisiGetir(){ // istenilirse oyuncuların bilgisini de getirebilirler.
+        // heyyy
         System.out.println("Oyuncu ismi soyismi: "+isimSoyisim+" Saldırı Puanı: "+saldiriGucu+" Savunma puanı: "+savunmaGucu);
     }
 }
